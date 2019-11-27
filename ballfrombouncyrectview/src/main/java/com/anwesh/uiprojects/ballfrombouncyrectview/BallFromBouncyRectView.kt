003b560d1cd7f69ccb7882f17abec09d2d128147
Anwesh43 <anweshthecool0@gmail.com>
2019-11-27 09:54:32 +0530

    adding constants and libraries

--- ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
+++ ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.ballfrombouncyrectview
  * Created by anweshmishra on 27/11/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
+
