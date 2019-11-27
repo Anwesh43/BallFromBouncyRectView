9379d5651555357a60add3a12aa5b11e48b1145c
Anwesh43 <anweshthecool0@gmail.com>
2019-11-27 10:08:54 +0530

    adding BFBNode class

--- ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
+++ ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
@@ -113,4 +113,46 @@ class BallFromBouncyRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFBNode(var i : Int, val state : State = State()) {
+
+        private var next : BFBNode? = null
+        private var prev : BFBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BFBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFBRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFBNode {
+            var curr : BFBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
