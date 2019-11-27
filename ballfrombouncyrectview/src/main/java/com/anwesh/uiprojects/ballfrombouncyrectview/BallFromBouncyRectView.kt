9de3592c64ab92f48e932c3277de76da3400ef72
Anwesh43 <anweshthecool0@gmail.com>
2019-11-27 10:02:08 +0530

    adding BallFromBouncyRectView class

--- ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
+++ ballfrombouncyrectview/src/main/java/com/anwesh/uiprojects/ballfrombouncyrectview/BallFromBouncyRectView.kt
@@ -48,3 +48,21 @@ fun Canvas.drawBFBRNode(i : Int, scale : Float, paint : Paint) {
     drawBallFromBouncyRect(scale, size, h, paint)
     restore()
 }
+
+class BallFromBouncyRectView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
